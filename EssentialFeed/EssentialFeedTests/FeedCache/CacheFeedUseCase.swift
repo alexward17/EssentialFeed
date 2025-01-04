a98f2f14c339c64b92d3279cbb260c29b4abfbff
alex1279 <alex@playlog.com>
2025-01-04 15:45:46 -0500

    ensure completion does not run when SUT instance is deallocated

--- EssentialFeed/EssentialFeedTests/FeedCache/CacheFeedUseCase.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/CacheFeedUseCase.swift
@@ -26,7 +26,8 @@ class LocalFeedLoader {
 
     func save(_ items: [FeedItem], completion: @escaping ((Error?) -> Void)) {
         store.deleteCachedFeed { [weak self] error in
-            guard let self, error == nil else {
+            guard let self else { return }
+            guard error == nil else {
                 completion(error)
                 return
             }
@@ -108,6 +109,16 @@ class CacheFeedUseCase: XCTestCase {
             store.completeDeletionSuccessfully()
             store.completeInsertionSuccessfully()
         })
+    }
+
+    func test_save_doesNotDeliverDeletionErrorWhenAfterSUTInstanceIsDeallocated() {
+        let store = FeedStoreSpy()
+        var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
+        var receivedResults: [Error?] = []
+        sut?.save([uniqueItem()], completion: { receivedResults.append($0) })
+        sut = nil
+        store.completeDeletion(with: Self.anyError)
+        XCTAssertTrue(receivedResults.isEmpty)
     }
 
     // MARK: - Helpers
