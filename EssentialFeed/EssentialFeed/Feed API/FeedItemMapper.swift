cd54570221314e48cce3d328f4a9bcbabef4e094
alex1279 <alex@playlog.com>
2025-01-05 14:38:39 -0500

    moved RemoteFeedItem to its own file

--- EssentialFeed/EssentialFeed/Feed API/FeedItemMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedItemMapper.swift
@@ -1,19 +1,5 @@
 import Foundation
 
-internal struct RemoteFeedItem: Codable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let image: URL
-
-    public init(id: UUID, description: String? = nil, location: String? = nil, image: URL) {
-        self.id = id
-        self.description = description
-        self.location = location
-        self.image = image
-    }
-}
-
 internal final class FeedItemMapper {
 
     private struct GetFeedItemsResponse: Decodable {
