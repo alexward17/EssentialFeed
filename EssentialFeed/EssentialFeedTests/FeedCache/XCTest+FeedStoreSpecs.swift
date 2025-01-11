d0c102b28c06914ccf80df793d046b5230829b76
alex1279 <alex@playlog.com>
2025-01-11 17:04:54 -0500

    Extracted reusable FeedStoreSpecs helpers into a shared scope such that they can be used by other FeedStore implementation tests

---
+++ EssentialFeed/EssentialFeedTests/FeedCache/XCTest+FeedStoreSpecs.swift
@@ -0,0 +1,77 @@
+//
+//  XCTest+FeedStoreSpecs.swift
+//  EssentialFeedTests
+//
+//  Created by Alex Ward on 2025-01-11.
+//
+
+import Foundation
+import EssentialFeed
+import XCTest
+
+extension FeedStoreSpecs where Self: XCTestCase {
+    @discardableResult func insert(_ cache: (feed: [LocalFeedImage], timestamp: Date), to sut: FeedStore) -> Error? {
+        let exp = expectation(description: "Await Completion")
+
+        var insertionError: Error?
+        sut.insert(cache.feed, timestamp: cache.timestamp) { receivedInsertionError in
+            insertionError = receivedInsertionError
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 1)
+        return insertionError
+    }
+
+    @discardableResult func deleteCache(from sut: FeedStore) -> Error? {
+        let exp = expectation(description: "Await Completion")
+
+        var deletionError: Error?
+        sut.deleteCachedFeed { receivedDeletionError in
+            deletionError = receivedDeletionError
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 1)
+        return deletionError
+    }
+
+    func expect(
+        _ sut: FeedStore,
+        toRetrieveTwice expectedResult: RetrieveCachedFeedResult,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        expect(sut, toRetrieve: expectedResult, file: file, line: line)
+        expect(sut, toRetrieve: expectedResult, file: file, line: line)
+    }
+
+    func expect(
+        _ sut: FeedStore,
+        toRetrieve expectedResult: RetrieveCachedFeedResult,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        let exp = expectation(description: "Wait for cache retrieval")
+
+        sut.retrieve { retrievedResult in
+            switch (expectedResult, retrievedResult) {
+
+            case (.empty, .empty),
+                (.failure, .failure):
+                break
+
+            case let (.found(feed: expectedFeed, timestamp: expectedTimestamp),
+                      .found(feed: retrievedFeed, timestamp: retrievedTimestamp)):
+                XCTAssertEqual(expectedFeed, retrievedFeed, file: file, line: line)
+                XCTAssertEqual(expectedTimestamp, retrievedTimestamp, file: file, line: line)
+
+            default:
+                XCTFail("Unexpected result: \(retrievedResult)", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 1)
+    }
+}
