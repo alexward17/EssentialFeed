81d5e698eef1ca984fb5db09bc133907fd520d31
alex1279 <alex@playlog.com>
2025-01-12 15:48:53 -0500

    include memory leak tracking helper in the integration test target + ensured LocalFeedLoader in integration with the CoreDataFeedStore deliver no items on empty cache

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -4,11 +4,89 @@
 //
 //  Created by Alex Ward on 2025-01-12.
 //
+// Integrate all cache module objects and see how they behave when collaborating
 
 import XCTest
+import EssentialFeed
 
 class EssentialFeedCacheIntegrationTests: XCTestCase {
 
-    
+    func test_load_deliversNoItemsOnEmptyCache() {
+        let sut = makeSUT()
+
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            guard case let .success(imageFeed) = result else {
+                XCTFail("Expected sucessful feed result")
+                return
+            }
+            XCTAssertEqual(imageFeed, [])
+            exp.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1)
+    }
+
+
+
+    // MARK: Helpers
+
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let storeURL = testSpecificStoreURL()
+        let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+        let sut = LocalFeedLoader(store: store, currentDate: Date.init)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+
+    private func save(_ feed: [FeedImage], with loader: LocalFeedLoader, file: StaticString = #file, line: UInt = #line) {
+        let saveExp = expectation(description: "Wait for save completion")
+        loader.save(feed) { saveError in
+            XCTAssertNil(saveError, "Expected to save feed successfully", file: file, line: line)
+            saveExp.fulfill()
+        }
+        wait(for: [saveExp], timeout: 1.0)
+    }
+
+    private func expect(_ sut: LocalFeedLoader, toLoad expectedFeed: [FeedImage], file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result {
+            case let .success(loadedFeed):
+                XCTAssertEqual(loadedFeed, expectedFeed, file: file, line: line)
+
+            case let .failure(error):
+                XCTFail("Expected successful feed result, got \(error) instead", file: file, line: line)
+            }
+
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+    }
+
+    // In Integration Test, we prefer to use a stack of production instances, with no test doubles
+    // This includes a physical file URL to make sure we can create and load the CoreData SQLite artifacts to disk, which can slow down the tests
+    // In Unit/Isolated Tests, we prefer to run operations in-memory when possible, which should be ultra-fast.
+    private func testSpecificStoreURL() -> URL {
+        return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+    }
+
+    private func cachesDirectory() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
 
 }
