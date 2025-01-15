01f5295f84d83bc0a8418de8af2340a649676377
alex1279 <alex@playlog.com>
2025-01-15 14:16:06 -0500

    replaced multiple explicit completion calls with Result catching

--- EssentialFeed/EssentialFeed/Feed Feature/FeedCache/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedCache/CoreDataFeedStore.swift
@@ -26,41 +26,31 @@ public class CoreDataFeedStore: FeedStore {
 
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         perform { context in
-            do {
+            completion(Result(catching: {
                 try ManagedCache.find(in: context).map(context.delete).map(context.save)
-                completion(.success(Void()))
-            } catch {
-                completion(.failure(error))
-            }
+            }))
         }
     }
 
     public func insert(_ feed: [EssentialFeed.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         perform { context in
-            do {
+            completion(Result(catching: {
                 let managedCache = try ManagedCache.newUniqueInstance(in: context)
                 managedCache.timestamp = timestamp
                 managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
 
                 try context.save()
-                completion(.success(Void()))
-            } catch {
-                completion(.failure(error))
-            }
+            }))
         }
     }
 
     public func retrieve(completion: @escaping RetrievalCompletion) {
         perform { context in
-            do {
-                guard let cache = try ManagedCache.find(in: context) else {
-                    completion(.success(.none))
-                    return
-                }
-                completion(.success((CachedFeed(feed: cache.localFeed, timestamp: cache.timestamp))))
-            } catch {
-                completion(.failure(error))
-            }
+            completion(Result(catching: {
+                try ManagedCache.find(in: context).map({
+                    return (CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp))
+                })
+            }))
         }
     }
 
