65ab1d7faf15a4d4ddda3122a2423dde0e626c51
alex1279 <alex@playlog.com>
2025-01-18 15:50:42 -0500

    render hard coded data on tableview

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -8,12 +8,23 @@ struct FeedImageViewModel {
 
 final class FeedViewController: UITableViewController {
 
+    private let feed = FeedImageViewModel.prototypeFeed
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        FeedImageViewModel.prototypeFeed.count
+        feed.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        tableView.dequeueReusableCell(withIdentifier: "FeedImageCell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(String(describing: FeedImageCell.self))) as? FeedImageCell ?? FeedImageCell()
+
+        let feedData = feed[indexPath.row]
+
+        cell.descriptionLabel.text = feedData.description
+        cell.locationLabel.text = feedData.location
+        cell.descriptionLabel.isHidden = feedData.description == nil
+        cell.locationLabel.isHidden = feedData.location == nil
+        cell.feedImageView.image = UIImage(named: feedData.imageName)
+        return cell
     }
 
 }
