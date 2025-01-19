d7f9803cc7024bff160d40916e8808700e8e1172
alex1279 <alex@playlog.com>
2025-01-19 11:28:40 -0500

    removed and re-added EssentialFeediOS framework + test folder

---
+++ EssentialFeed/EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialFeediOSTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Alex Ward on 2025-01-19.
+//
+
+import XCTest
+@testable import EssentialFeed
+@testable import EssentialFeediOS
+
+final class EssentialFeediOSTests: XCTestCase {
+
+}
