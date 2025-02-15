fa2445020bc2e1359de60b7e8f6b785b1772c967
alex1279 <alex@playlog.com>
2025-02-15 16:21:50 -0500

    extracted adapted pattern to function to clarify intent

--- EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -1,12 +1,23 @@
 public final class FeedUIComposer {
+
     private init() {}
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let refreshController = FeedRefreshViewController(feedLoader: feedLoader)
         let feedController = FeedViewController(refreshController: refreshController)
-        feedController.refreshController?.onRefresh = { [weak feedController] feed in
-            feedController?.tableModel = feed.map({ FeedImageCellController(model: $0, imageLoader: imageLoader) })
-        }
+
+        feedController.refreshController?.onRefresh = adaptFeedToCellControllers(forwardingTo: feedController, loader: imageLoader)
+
         return feedController
+    }
+
+    // THIS CLOSURE IS THE ADDAPTER PATTERN, it allows `adaptation` of un-matching APIs.
+    // In thise case onRefresh delivers [FeedImage] that are adapted to [FeedImageCellController] on which the FeedViewController depends
+    private static func adaptFeedToCellControllers(
+        forwardingTo controller: FeedViewController,
+        loader: FeedImageDataLoader) -> ([FeedImage]) -> Void {
+            return { [weak controller] feed in
+                controller?.tableModel = feed.map({ FeedImageCellController(model: $0, imageLoader: loader) })
+            }
     }
 
 }
