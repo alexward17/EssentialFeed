9abc22aec63d2352b6b30d633e1eaf2ec0841347
alex1279 <alex@playlog.com>
2025-02-22 12:36:21 -0500

    made prototy cell outlets optional

--- EssentialFeed/EssentialFeedPrototype/Cells/PrototypeFeedImageCell.swift
+++ EssentialFeed/EssentialFeedPrototype/Cells/PrototypeFeedImageCell.swift
@@ -8,41 +8,41 @@
 import UIKit
 
 final class PrototypeFeedImageCell: UITableViewCell {
-    @IBOutlet private(set) var locationContainer: UIView!
-    @IBOutlet private(set) var locationLabel: UILabel!
-    @IBOutlet private(set) var feedImageView: UIImageView!
-    @IBOutlet private(set) var feedImageContainer: UIView!
-    @IBOutlet private(set) var descriptionLabel: UILabel!
+    @IBOutlet private(set) var locationContainer: UIView?
+    @IBOutlet private(set) var locationLabel: UILabel?
+    @IBOutlet private(set) var feedImageView: UIImageView?
+    @IBOutlet private(set) var feedImageContainer: UIView?
+    @IBOutlet private(set) var descriptionLabel: UILabel?
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        feedImageView.alpha = 0
-        feedImageContainer.startShimmering()
+        feedImageView?.alpha = 0
+        feedImageContainer?.startShimmering()
     }
 
     override func prepareForReuse() {
         super.prepareForReuse()
 
-        feedImageView.alpha = 0
-        feedImageContainer.startShimmering()
+        feedImageView?.alpha = 0
+        feedImageContainer?.startShimmering()
     }
 
     func fadeInFeedImage(_ image: UIImage?) {
-        feedImageView.image = image
+        feedImageView?.image = image
         UIView.animate(withDuration: 0.3, delay: 0.3, animations: { [weak self] in
-            self?.feedImageView.alpha = 1
+            self?.feedImageView?.alpha = 1
         }, completion: { [weak self] _ in
-            self?.feedImageContainer.stopShimmering()
+            self?.feedImageContainer?.stopShimmering()
         })
     }
 }
 
 extension PrototypeFeedImageCell {
     func configuew(with model: PrototypeFeedImageViewModel) {
-        descriptionLabel.text = model.description
-        locationLabel.text = model.location
-        descriptionLabel.isHidden = model.description == nil
-        locationLabel.isHidden = model.location == nil
+        descriptionLabel?.text = model.description
+        locationLabel?.text = model.location
+        descriptionLabel?.isHidden = model.description == nil
+        locationLabel?.isHidden = model.location == nil
         fadeInFeedImage(UIImage(named: model.imageName))
     }
 }
