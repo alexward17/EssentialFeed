272c6cdb4e5b7b0d321442922ecd06768ff4d37b
alex1279 <alex@playlog.com>
2025-03-22 11:45:22 -0500

    Moved production types to separate production file

--- EssentialFeed/EssentialFeedTests/FeedPresentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedPresentation/FeedPresenterTests.swift
@@ -1,39 +1,6 @@
 import XCTest
 import EssentialFeed
 
-public struct FeedErrorViewModel {
-    public let message: String?
-
-    static var noError: FeedErrorViewModel {
-        return FeedErrorViewModel(message: nil)
-    }
-
-    static func error(message: String) -> FeedErrorViewModel {
-        return FeedErrorViewModel(message: message)
-    }
-}
-
-public struct FeedViewModel {
-    let feed: [FeedImage]
-}
-
-public protocol FeedView {
-    func display(_ viewModel: FeedViewModel)
-}
-
-public struct FeedLoadingViewModel {
-    let isLoading: Bool
-}
-
-public protocol FeedLoadingView {
-    func display(_ viewModel: FeedLoadingViewModel)
-}
-
-public protocol FeedErrorView {
-    func display(_ viewModel: FeedErrorViewModel)
-}
-
-
 class FeedPresenterTests: XCTestCase {
 
     func test_titleIsLocalized() {
@@ -94,49 +61,6 @@ class FeedPresenterTests: XCTestCase {
         }
 
         return value
-    }
-
-    private class FeedPresenter {
-
-        final var feedView: FeedView
-        final var loadingView: FeedLoadingView
-        final let errorView: FeedErrorView
-
-        static var title: String {
-            return NSLocalizedString("FEED_VIEW_TITLE",
-                    tableName: "Feed",
-                    bundle: Bundle(for: FeedPresenter.self),
-                    comment: "Error message displayed when we can't load the image feed from the server")
-        }
-
-
-        private var feedLoadError: String {
-            return NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
-                    tableName: "Feed",
-                    bundle: Bundle(for: FeedPresenter.self),
-                    comment: "Error message displayed when we can't load the image feed from the server")
-        }
-
-        init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedErrorView) {
-            self.feedView = feedView
-            self.loadingView = loadingView
-            self.errorView = errorView
-        }
-
-        func didStartLoadingFeed() {
-            errorView.display(.noError)
-            loadingView.display(FeedLoadingViewModel(isLoading: true))
-        }
-
-        final func didFinishLoadingFeed(with feed: [FeedImage]) {
-            feedView.display(FeedViewModel(feed: feed))
-            loadingView.display(FeedLoadingViewModel(isLoading: false))
-        }
-
-        final func didFinishLoadingFeed(with error: Error) {
-            errorView.display(.error(message: feedLoadError))
-            loadingView.display(FeedLoadingViewModel(isLoading: false))
-        }
     }
 
     private class ViewSpy: FeedErrorView, FeedLoadingView, FeedView {
