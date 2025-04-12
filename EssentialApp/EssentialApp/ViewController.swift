ea0732a4d4085bdb1ff7d33dafeaf8b7a333849c
alex1279 <alex@playlog.com>
2025-04-12 09:46:26 -0500

    Created EssentialApp project, combined EssentialFeed.xcodeproj into workspace

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Alex Ward on 2025-04-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
