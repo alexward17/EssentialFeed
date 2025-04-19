cbde55d2f188d2ab448931de929f42ad7d929962
alex1279 <alex@playlog.com>
2025-04-19 11:00:32 -0500

    added FeedImageDataLoaderCacheDecorator and tests

--- EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -1,65 +1,39 @@
-//
-//  RemoteWithLocalFallbackFeedLoaderTests.swift
-//  EssentialAppTests
-//
-//  Created by Alex Ward on 2025-04-18.
-//
-
 import XCTest
 import EssentialFeed
 import EssentialApp
 
 class FeedLoaderWithFallbackCompositeTests: XCTestCase, FeedLoaderTestCase {
 
-    func test_load_deliversPrimaryFeedOnPrimaryLoadSuccess() {
+    func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
         let primaryFeed = uniqueFeed()
         let fallbackFeed = uniqueFeed()
+        let sut = makeSUT(primaryResult: .success(primaryFeed), fallbackResult: .success(fallbackFeed))
 
-        expect(
-            makeSUT(
-                primaryResult: .success(primaryFeed),
-                fallbackResult: .success(fallbackFeed)),
-            toCompleteWith: .success(primaryFeed)
-        )
+        expect(sut, toCompleteWith: .success(primaryFeed))
     }
 
-    func test_load_deliversFallbackFeedOnPrimaryLoadFailure() {
+    func test_load_deliversFallbackFeedOnPrimaryLoaderFailure() {
         let fallbackFeed = uniqueFeed()
+        let sut = makeSUT(primaryResult: .failure(Self.anyNSError), fallbackResult: .success(fallbackFeed))
 
-        expect(
-            makeSUT(
-                primaryResult: .failure(Self.anyNSError),
-                fallbackResult: .success(fallbackFeed)),
-            toCompleteWith: .success(fallbackFeed)
-        )
+        expect(sut, toCompleteWith: .success(fallbackFeed))
     }
 
-    func test_load_deliversErrorOnPrimaryAndFallbackFailure() {
-        expect(
-            makeSUT(
-                primaryResult: .failure(Self.anyNSError),
-                fallbackResult: .failure(Self.anyNSError)),
-            toCompleteWith: .failure(Self.anyNSError)
-        )
+    func test_load_deliversErrorOnBothPrimaryAndFallbackLoaderFailure() {
+        let sut = makeSUT(primaryResult: .failure(Self.anyNSError), fallbackResult: .failure(Self.anyNSError))
+
+        expect(sut, toCompleteWith: .failure(Self.anyNSError))
     }
 
     // MARK: - Helpers
 
-    private final func makeSUT(
-        primaryResult: FeedLoader.Result,
-        fallbackResult: FeedLoader.Result,
-        file: StaticString = #file,
-        line: UInt = #line
-    ) -> FeedLoaderWithFallbackComposite {
-
+    private func makeSUT(primaryResult: FeedLoader.Result, fallbackResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) -> FeedLoader {
         let primaryLoader = FeedLoaderStub(result: primaryResult)
         let fallbackLoader = FeedLoaderStub(result: fallbackResult)
         let sut = FeedLoaderWithFallbackComposite(primaryLoader: primaryLoader, fallbackLoader: fallbackLoader)
-
         trackForMemoryLeaks(primaryLoader, file: file, line: line)
         trackForMemoryLeaks(fallbackLoader, file: file, line: line)
-        trackForMemoryLeaks(primaryLoader, file: file, line: line)
-
+        trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
 
