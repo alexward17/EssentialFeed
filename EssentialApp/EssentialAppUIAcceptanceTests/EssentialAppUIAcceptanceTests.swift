31b303ad387292d6107a84d032339404b0661e13
alex1279 <alex@playlog.com>
2025-04-26 16:50:30 -0500

    Display empty feed when there is no connectivity and no cache

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -10,6 +10,7 @@ import XCTest
 final class EssentialAppUIAcceptanceTests: XCTestCase {
 
 
+
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
         let app = XCUIApplication()
 
@@ -35,6 +36,17 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
 
         let firstImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(firstImage.exists)
+    }
+
+    func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivityandNoCache() {
+        let app = XCUIApplication()
+
+        app.launchArguments = ["-reset", "-connectivity", "offline"]
+
+        app.launch()
+
+        let feedCells = app.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(feedCells.count, 0)
     }
 
 }
