865ebfd065f15cad13d0d77b85f19fb8438581d1
alex1279 <alex@playlog.com>
2025-04-26 15:55:51 -0500

    Added test target for running hig level acceptance tests

---
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialAppUIAcceptanceTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by Alex Ward on 2025-04-26.
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTests: XCTestCase {
+
+}
