8c512f365597c6390f44ee01ac96f45658d72cae
alex1279 <alex@playlog.com>
2025-04-26 16:13:19 -0500

    Revert "removed duplicate test plan file references"
    This reverts commit 99da9a6633746700305260577b3b23afee0d5449.

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -9,9 +9,4 @@ import XCTest
 
 final class EssentialAppUIAcceptanceTests: XCTestCase {
 
-
-    func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
-
-    }
-
 }
---
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTestsLaunchTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialAppUIAcceptanceTestsLaunchTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by Alex Ward on 2025-04-26.
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTestsLaunchTests: XCTestCase {
+
+}
