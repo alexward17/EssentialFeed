f7dd1a6986b8a2dfa8bf08a83f828de1e86e2bfb
alex1279 <alex@playlog.com>
2025-05-04 15:32:40 -0500

    Dublicate remote feed loader as remote image comments loader

--- EssentialFeed/EssentialFeed/Feed API/ImageCommentsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/ImageCommentsMapper.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-final class FeedItemsMapper {
+public final class ImageCommentsMapper {
     private struct Root: Decodable {
         let items: [RemoteFeedItem]
     }
 
     static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
         guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
-            throw RemoteFeedLoader.Error.invalidData
+            throw RemoteImageCommentsLoader.Error.invalidData
         }
 
         return root.items
---
+++ EssentialFeed/EssentialFeed/Feed API/RemoteImageCommentsLoader.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+public final class RemoteImageCommentsLoader {
+
+    // MARK: - Types
+
+    public typealias Result = FeedLoader.Result
+
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+
+    // MARK: - Properties
+
+    private final let url: URL
+    private final let client: HTTPClient
+
+    // MARK: - Initializers
+
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+
+    // MARK: - Loading Functions
+
+    public func load(completion: @escaping (Result) -> Void) {
+        client.get(from: url, completion: { [weak self] result in
+            guard self != nil else { return }
+            switch result {
+            case let .success((data, response)):
+                completion(Self.map(data, response))
+            case .failure:
+                completion(.failure(Error.connectivity))
+            }
+        })
+    }
+
+    private static func map(_ data: Data, _ response: HTTPURLResponse) -> Result {
+        do {
+            let items = try ImageCommentsMapper.map(data, from: response)
+            return .success(items.toModels())
+        } catch {
+            return .failure(error)
+        }
+    }
+
+}
+
+private extension Array where Element == RemoteFeedItem {
+    func toModels() -> [FeedImage] {
+        map({ FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.image) })
+    }
+
+}
